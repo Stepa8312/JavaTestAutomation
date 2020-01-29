f4e9a11e565fdb7f876b5c74bd6373fd143766b1
Nikolay Stepanets <nik.stepanets@gmail.com>
2020-01-29 11:15:46 +0200

    Added Firefox driver

--- src/test/java/parentTest/ParentTest.java
+++ src/test/java/parentTest/ParentTest.java
@@ -2,6 +2,8 @@ package parentTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import pages.HomePage;
@@ -12,14 +14,24 @@ import java.util.concurrent.TimeUnit;
 
 public class ParentTest {
     WebDriver webDriver;
+    String browser = "chrome";
     protected LoginPage loginPage;
     protected HomePage homePage;
 
     @BeforeTest
     public void setUp() {
-        File file = new File("./src/drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        webDriver = new ChromeDriver();
+        if ("chrome".equals(browser) || browser == null) {
+            File file = new File("./src/drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+            webDriver = new ChromeDriver();
+        } else if ("firefox".equals(browser)) {
+            File file = new File("./src/drivers/geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+            FirefoxOptions profile = new FirefoxOptions();
+            profile.addPreference("browser.startup.page", 0);
+            profile.addPreference("browser.startup.homepage_override.mstone", "ignore");
+            webDriver = new FirefoxDriver();
+        }
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         loginPage = new LoginPage(webDriver);
